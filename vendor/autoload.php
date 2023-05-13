98f43b7b57f5d02628d85174af022f93edc3dd24
valentioaditama <valentioaditama8@gmail.com>
2023-05-14 00:43:37 +0700

    fixing laravel error

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -22,4 +22,4 @@ if (PHP_VERSION_ID < 50600) {
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInitab68119becebd19ba547e9e3737e8aa5::getLoader();
+return ComposerAutoloaderInit0fa9e30246ef5776be0debed1883a1b9::getLoader();
--- vendor/laravel/framework/src/Illuminate/Support/Carbon.php
+++ vendor/laravel/framework/src/Illuminate/Support/Carbon.php
@@ -4,9 +4,12 @@ namespace Illuminate\Support;
 
 use Carbon\Carbon as BaseCarbon;
 use Carbon\CarbonImmutable as BaseCarbonImmutable;
+use Illuminate\Support\Traits\Conditionable;
 
 class Carbon extends BaseCarbon
 {
+    use Conditionable;
+
     /**
      * {@inheritdoc}
      */
@@ -14,5 +17,28 @@ class Carbon extends BaseCarbon
     {
         BaseCarbon::setTestNow($testNow);
         BaseCarbonImmutable::setTestNow($testNow);
+    }
+
+    /**
+     * Dump the instance and end the script.
+     *
+     * @param  mixed  ...$args
+     * @return never
+     */
+    public function dd(...$args)
+    {
+        dd($this, ...$args);
+    }
+
+    /**
+     * Dump the instance.
+     *
+     * @return $this
+     */
+    public function dump()
+    {
+        dump($this);
+
+        return $this;
     }
 }
