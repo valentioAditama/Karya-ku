afc9a80f7fdeb2b04cf040d94f1348b35584feed
valentioaditama <valentioaditama8@gmail.com>
2023-05-14 23:02:38 +0700

    landing page progress

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -1,5 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-
+<div class="background-landing-page text-light">
+    <div class="container d-flex justify-content-end align-items-center h-100">
+        <div class="row">
+            <div class="col-md-12">
+                <p class="fs-2">Selamat Datang Di <b>Karya-ku</b></p>
+            </div>
+        </div>
+    </div>
+</div>
 @endSection
